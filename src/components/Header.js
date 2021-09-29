8893f6685ec95c66318c506d4f924f78146e3b4b
Estu Ramadian Wiharto <weramadian@gmail.com>
2021-09-30 00:37:48 +0700

    Add header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="container-fluid bg-dark">
+      <header className="d-flex justify-content-center py-2">
+        <a className="navbar-brand" href="/">
+          <h2 className="text-white">Book of Dog</h2>
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
